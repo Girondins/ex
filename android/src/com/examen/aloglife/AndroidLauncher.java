fbb660623c45c86638cd5432227c2888655ee685
Girondins <stmsfsd@gmail.com>
2017-02-28 13:36:09 +0100

    Initial commit

---
+++ android/src/com/examen/aloglife/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.examen.aloglife;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.examen.aloglife.libgdxrun;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SimpleTest1(), config);
+	}
+}
